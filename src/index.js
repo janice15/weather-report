642d4609d4f47399d1dc695561b4f782d37e39b7
Janice <janicenguyenn@gmail.com>
2023-06-09 12:52:14 -0700

    add code for increase and decrease buttons

---
+++ src/index.js
@@ -0,0 +1,12 @@
+//const state = {
+//    default: 0,
+//};
+
+const minusTemp = document.querySelector('.minus')
+const plusTemp = document.querySelector('.plus');
+
+plus.addEventListener('click',() => document.getElementById("number").textContent++);
+
+minus.addEventListener('click',() => document.getElementById("number").textContent--);
+
+document.addEventListener("DOMContentLoaded", registerEventHandlers);
