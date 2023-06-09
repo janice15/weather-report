54cd3a96b9b94f088fa956b6b70333fe8e3d4cf2
Janice <janicenguyenn@gmail.com>
2023-06-09 13:01:33 -0700

    fix + and - buttons

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
-//const state = {
-//    default: 0,
-//};
-
 const minusTemp = document.querySelector('.minus')
 const plusTemp = document.querySelector('.plus');
 
-plus.addEventListener('click',() => document.getElementById("number").textContent++);
+plusTemp.addEventListener('click',() => document.getElementById("number").textContent++);
 
-minus.addEventListener('click',() => document.getElementById("number").textContent--);
-
+minusTemp.addEventListener('click',() => document.getElementById("number").textContent--);
 document.addEventListener("DOMContentLoaded", registerEventHandlers);
