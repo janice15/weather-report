8b13b7ae1fe6bc4e6c7d51ec0643fe0be31b7d03
Edith <mxedithbaltazar@gmail.com>
2023-06-09 18:25:47 -0700

    add reset button, input for city and updated js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
+
+function resetInput() {
+  document.getElementById("textInput").value = "Seattle";
+}
+
+document.getElementById("textInput").addEventListener("input", function() {
+  document.getElementById("output").innerHTML = this.value;
+});
+
+
+
 const minusTemp = document.querySelector('.minus')
 const plusTemp = document.querySelector('.plus');
 
 plusTemp.addEventListener('click',() => document.getElementById("number").textContent++);
 
 minusTemp.addEventListener('click',() => document.getElementById("number").textContent--);
+
+
 document.addEventListener("DOMContentLoaded", registerEventHandlers);
+
