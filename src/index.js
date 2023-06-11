618d3e0ae207f4a1d585ed185efbac212ae64b81
Janice <janicenguyenn@gmail.com>
2023-06-11 15:57:24 -0700

    implement code to change temperature colors

--- src/index.js
+++ src/index.js
@@ -2,27 +2,53 @@ const state = {
     temp: 72,
     city: "Seattle"
 };
+const changeTempAndLandscape = () => {
+    let temp = state.temp;
+    let color = 'red';
+    let landscape = '🌵__🐍_🦂_🌵🌵__🐍_🏜';
+    if (temp > 80) {
+        color = 'red';
+        landscape = '🌵__🐍_🦂_🌵🌵__🐍_🏜';
+    } else if (temp > 70) {
+        color = 'orange';
+        landscape = '🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻';
+    } else if (temp > 60) {
+        color = 'yellow';
+        landscape = '🌾🌾_🍃_🪨__🛤_🌾🌾🌾';
+    } else if (temp > 50) {
+        color = 'yellow-green';
+        landscape = '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️';
+    } else {
+        color = 'blue';
+        landscape = '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️';
+    }
 
+    const newLandscape = document.getElementById('landscape');
+    newLandscape.textContent = landscape;
+    const temperature = document.getElementById('defaultTemp');
+    temperature.className = color;
+    temperature.textContent = String(state.temp);
+};
 const increaseTemp = () => {
     state.temp += 1;
-    const CountContainer = document.getElementById('defaultTemp')
-    CountContainer.textContent = `${state.temp}`;
+    changeTempAndLandscape();
 };
 const decreaseTemp = () => {
     state.temp -= 1;
-    const CountContainer = document.getElementById('defaultTemp')
-    CountContainer.textContent = `${state.temp}`;
+    changeTempAndLandscape();
+
 };
 
 const registerEventHandlers = () => {
+    changeTempAndLandscape();
     const increaseButton = document.getElementById('increaseTempButton');
     increaseButton.addEventListener("click", increaseTemp);
     const decreaseButton = document.getElementById('decreaseTempButton');
     decreaseButton.addEventListener("click", decreaseTemp);
 
-resetSky();
-const skySelect = document.getElementById('skySelect');
-skySelect.addEventListener('change', updateSky);
+    resetSky();
+    const skySelect = document.getElementById('skySelect');
+    skySelect.addEventListener('change', updateSky);
 };
 
 
